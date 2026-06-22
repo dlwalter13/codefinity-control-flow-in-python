9122325bcaea1c9bfb0fcce534617e52f7c59ff5
dlwalter13 <>
2026-06-22 21:13:32 +0000

    No Content

---
+++ mastering_python_if_else_statements/no_content/main.py
@@ -0,0 +1,7 @@
+water_intake = 1200
+daily_goal = 2000
+
+if water_intake >= daily_goal:
+    print("Great job! You've reached your water goal!")
+else:
+    print("You haven't reached your water goal yet!")
